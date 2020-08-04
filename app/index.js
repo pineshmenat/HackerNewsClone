2032243d3ea5ba5353adaa423c2109ea81d72bd1
pineshmenat <pineshmenat@gmail.com>
2020-08-04 02:03:12 -0400

    Default to dark theme

--- app/index.js
+++ app/index.js
@@ -11,7 +11,7 @@ const Post = React.lazy(() => import('./components/Post'));
 const User = React.lazy(()=> import('./components/User'));
 
 function App(){
-    const [theme, setTheme] = useState("light");
+    const [theme, setTheme] = useState("dark");
 
     const toggleTheme = () => {
         setTheme((theme) => (theme === "light" ? "dark" : "light") );
